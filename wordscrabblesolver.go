38ed3db9c83a701119de9f450ba977b3794b8a50
nmorgan <morgan.nogues@ynov.com>
2023-09-23 00:01:37 +0200

    Add new Branch CompareRow and new Func CompareGrid initializ

--- wordscrabblesolver.go
+++ wordscrabblesolver.go
@@ -43,21 +43,44 @@ func sep() {
 	}
 }
 
-func Compare(field [10][10]rune, wordList []string) bool {
-	row := 0
-	grid := 0
-	elems := 0
-	char := 0
-	for row < 10 {
-		for grid < 10 {
-			for {
-				if wordList[elems][char] == byte(field[row][grid]) {
-
+func Compare(field [10][10]rune, wordList []string) []string { // fonction pour comparer les mots entre le tableau et le fichier
+	wordFound := []string{}         // liste des mots trouvés
+	for _, word := range wordList { // parcours des mots dans le fichier
+		for row := 0; row < 10; row++ { // parcours les lignes du tableau
+			for grid := 0; grid < 10; grid++ { // parcours les colonnes du tableau
+				if rune(word[0]) == field[row][grid] { // si la première lettre du mot correspond à la première lettre du table
+					char := 1                                                                                          // compte le nombre de caractères restants dans le mot
+					for char < len(word) && grid+char < len(field[row]) && rune(word[char]) == field[row][grid+char] { // parcours les caractères restants dans le mot
+						char++ // compte le nombre de caractères restants dans le mot
+					}
+					if char == len(word) { // si le nombre de caractères restants dans le mot correspond à la taille du mot
+						wordFound = append(wordFound, word) // ajoute le mot trouvé dans la liste des mots trouvés
+					}
 				}
 			}
 		}
 	}
-	return false
+	return wordFound // retourne la liste des mots trouvés
+}
+
+func CompareGrid(field [10][10]rune, wordList []string) []string { // fonction pour comparer les mots entre le tableau et le fichier
+	wordFound := []string{}         // liste des mots trouvés
+	for _, word := range wordList { // parcours des mots dans le fichier
+		for grid := 0; grid < 10; grid++ { // parcours les lignes du tableau
+			for row := 0; row < 10; row++ { // parcours les colonnes du tableau
+				if rune(word[0]) == field[grid][row] { // si la première lettre du mot correspond à la première lettre du table
+					char := 1                                                                                          // compte le nombre de caractères restants dans le mot
+					for char < len(word) && row+char < len(field[grid]) && rune(word[char]) == field[grid][row+char] { // parcours les caractères restants dans le mot
+						char++ // compte le nombre de caractères restants dans le mot
+					}
+					if char == len(word) { // si le nombre de caractères restants dans le mot correspond à la taille du mot
+						wordFound = append(wordFound, word) // ajoute le mot trouvé dans la liste des mots trouvés
+					}
+				}
+			}
+		}
+	}
+	return wordFound // retourne la liste des mots trouvés
 }
 
 func ReadFile() []string {
